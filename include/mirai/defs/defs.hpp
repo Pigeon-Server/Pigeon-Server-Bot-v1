b9cd3eb9b497715f41d22d27f5814fd2f4521a1c
Half_nothing <50048293+half-nothing@users.noreply.github.com>
2022-05-02 13:32:29 +0800

    Add files via upload

---
+++ include/mirai/defs/defs.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef mirai_cpp_defs_defs_hpp_H_
+#define mirai_cpp_defs_defs_hpp_H_
+
+#include "ISerializable.hpp"
+#include "QQType.hpp"
+#include "Friend.hpp"
+#include "Group.hpp"
+#include "GroupMember.hpp"
+#include "GroupConfig.hpp"
+#include "GroupFile.hpp"
+#include "MessageChain.hpp"
+#include "Profile.hpp"
+#include "FileDownloadInfo.hpp"
+#include "GroupAnnouncement.hpp"
+
+#endif // !mirai_cpp_defs_defs_hpp_H_
---
+++ include/mirai/exceptions/exceptions.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef mirai_cpp_exceptions_exceptions_hpp_H_
+#define mirai_cpp_exceptions_exceptions_hpp_H_
+
+#include "MiraiApiHttpException.hpp"
+#include "NetworkException.hpp"
+
+#endif // !mirai_cpp_exceptions_exceptions_hpp_H_
